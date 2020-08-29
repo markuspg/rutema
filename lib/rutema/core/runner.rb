2b4784b4bc6c376ae707e1f08de69a9ed590ccb9
markuspg <markuspg@users.noreply.github.com>
2020-08-29 15:09:10 +0200

    Revise runners.rb

--- lib/rutema/core/runner.rb
+++ lib/rutema/core/runner.rb
@@ -1,142 +1,166 @@
 # Copyright (c) 2007-2020 Vassilis Rizopoulos. All rights reserved.
 
-require_relative "framework"
+# frozen_string_literal: true
+
+require 'English'
+
+require_relative 'framework'
 
 module Rutema
-  module Runners    
-    class Default
+  ##
+  # Module for the definition of runners which can be used by Rutema::Engine to
+  # execute test specifications
+  #
+  # _rutema_ comes by default with two runners Rutema::Runners::Default and
+  # Rutema::Runners::NoOp
+  module Runners
+    ##
+    # An empty base class for all runner classes
+    class BaseRunner
+    end
+
+    ##
+    # Rutema::Runners::Default is the default runner used by Rutema::Engine
+    #
+    # As its name indicates its purpose is to run (i.e. execute) test
+    # specifications which is done through its #run method.
+    class Default < BaseRunner
       include Rutema::Messaging
 
+      ##
+      # The context of the runner (this class only stores 'rutema_status' in it)
       attr_reader :context
+      ##
+      # A specification to be run before any specification run with #run
       attr_accessor :setup
+      ##
+      # A specification to be run after any specification run with #run
       attr_accessor :teardown
 
-      def initialize context,queue
-        @setup=nil
-        @teardown=nil
-        @context=context || Hash.new
+      ##
+      # Initialize a new Rutema::Runners::Default with a +context+ and a +queue+
+      def initialize(context, queue)
+        @setup = nil
+        @teardown = nil
+        @context = context || {}
         @queue = queue
-        @number_of_runs=0
+        @number_of_runs = 0
         @cleanup_blocks = []
       end
 
-      def run spec, is_special = false
-        begin
-          @context["spec_name"]=spec.name
-          steps=[]
-          status=:success
-          state={'start_time'=>Time.now, "sequence_id"=>@number_of_runs,:test=>spec.name}
-          message(:test=>spec.name,:text=>'started')
-          if @setup
-            message(:test=>spec.name,:text=>'setup')
-            executed_steps,setup_status=run_scenario("_setup_",@setup.scenario,@context,true)
-            status=setup_status unless STATUS_CODES.find_index(setup_status) < STATUS_CODES.find_index(status)
-            steps+=executed_steps
-          end
-          if status!=:error
-            message(:test=>spec.name,:text=>'running')
-            executed_steps,testspec_status=run_scenario(spec.name,spec.scenario,@context,is_special)
-            status=testspec_status unless STATUS_CODES.find_index(testspec_status) < STATUS_CODES.find_index(status)
-            steps+=executed_steps
-          else
-            message(:test=>spec.name,'number'=>0,'status'=>:error,'out'=>"Setup failed",'err'=>"",'duration'=>0)
-          end
-          @context['rutema_status']=status
-          if @teardown
-            message(:test=>spec.name,:text=>'teardown')
-            executed_steps,teardown_status=run_scenario("_teardown_",@teardown.scenario,@context,true)
-            status=teardown_status unless STATUS_CODES.find_index(teardown_status) < STATUS_CODES.find_index(status)
-          end
-          @context['rutema_status']=status
-          message(:test=>spec.name,:text=>'finished')
-          state['status']=status
-          state["stop_time"]=Time.now
-          state['steps']=steps
-          @number_of_runs+=1
-          return state
-        ensure
-          begin
-            cleanup_exception = nil
-            @cleanup_blocks.each do |cleanup_block|
-              #Try all bocks
-              begin
-                cleanup_block.run(@context) if cleanup_block.respond_to?(:run)
-              rescue Exception => e
-                #Ignore errors, ensure all cleanup steps are attempted
-                cleanup_exception = e
-              end              
-            end
-            raise cleanup_exception if !cleanup_exception.nil?
-          ensure
-            @cleanup_blocks = []
-          end
+      def run(spec, is_special = false)
+        @context['spec_name'] = spec.name
+        steps = []
+        status = :success
+        state = { 'sequence_id' => @number_of_runs, 'start_time' => Time.now, test: spec.name }
+        message(test: spec.name, text: 'started')
+        if @setup
+          message(test: spec.name, text: 'setup')
+          executed_steps, setup_status = run_scenario('_setup_', @setup.scenario,
+                                                      @context, true)
+          status = setup_status unless STATUS_CODES.find_index(setup_status) < STATUS_CODES.find_index(status)
+          steps += executed_steps
         end
+        if status != :error
+          message(test: spec.name, text: 'running')
+          executed_steps, testspec_status = run_scenario(spec.name, spec.scenario,
+                                                         @context, is_special)
+          status = testspec_status unless STATUS_CODES.find_index(testspec_status) < STATUS_CODES.find_index(status)
+          steps += executed_steps
+        else
+          message(test: spec.name, 'duration' => 0, 'err' => '', 'number' => 0,
+                  'out' => 'Setup failed', 'status' => :error)
+        end
+        @context['rutema_status'] = status
+        if @teardown
+          message(test: spec.name, text: 'teardown')
+          _, teardown_status = run_scenario('_teardown_', @teardown.scenario,
+                                            @context, true)
+          status = teardown_status unless STATUS_CODES.find_index(teardown_status) < STATUS_CODES.find_index(status)
+        end
+        @context['rutema_status'] = status
+        message(test: spec.name, text: 'finished')
+        state['status'] = status
+        state['stop_time'] = Time.now
+        state['steps'] = steps
+        @number_of_runs += 1
+        state
       end
 
       private
 
-      def cache_cleanup step
-        if step.has_cleanup? && step.cleanup.respond_to?(:run)
-          @cleanup_blocks << step.cleanup
-        end
-      end
-
-      def run_scenario name,scenario,meta,is_special
-        executed_steps=[]
-        status=:skipped
-        begin 
-          stps=scenario.steps
+      ##
+      #
+      def run_scenario(name, scenario, meta, is_special)
+        executed_steps = []
+        status = :skipped
+        begin
+          stps = scenario.steps
           if stps.empty?
-            error(name,"Scenario #{name} contains no steps")
-            status=:error
+            error(name, "Scenario #{name} contains no steps")
+            status = :error
           else
             stps.each do |s|
-
-              if status == :error && s.skip_on_error?
-                message(:test=>name,:text=>s.to_s,'number'=>s.number,'status'=>:skipped,'is_special'=>is_special)
-              else
-                message(:test=>name,:text=>s.to_s,'number'=>s.number,'status'=>:started,'is_special'=>is_special)
-                sleep 0.05
-                begin
-                  cache_cleanup(s)
-                  executed_steps<<run_step(s,meta)
-                rescue Exception => e
-                  throw e unless s.continue?
-                  s.status = :error                
-                end
-                message(:test=>name,:text=>s.to_s,'number'=>s.number,'status'=>s.status,'out'=>s.output,'err'=>s.error,'backtrace'=>s.backtrace,'duration'=>s.exec_time,'is_special'=>is_special)
-                status=s.status unless STATUS_CODES.find_index(s.status) < STATUS_CODES.find_index(status)
-                break if :error==s.status and !s.continue?
+              message(test: name, text: s.to_s, 'number' => s.number,
+                      'status' => :started, 'is_special' => is_special)
+              sleep 0.05
+              begin
+                executed_steps << run_step(s, meta)
+                message(test: name, text: s.to_s, 'number' => s.number,
+                        'status' => s.status, 'out' => s.output, 'err' => s.error,
+                        'backtrace' => s.backtrace, 'duration' => s.exec_time,
+                        'is_special' => is_special)
+              rescue Exception => e
+                throw e unless s.continue?
+                s.status = :error
               end
+              # Status of lower "importance" my not cover higher importance ones
+              status = s.status unless STATUS_CODES.find_index(s.status) < STATUS_CODES.find_index(status)
+              break if s.status == :error && !s.continue?
             end
           end
-        rescue
-          error(name,$!.message)
-          status=:error
+        rescue StandardError
+          error(name, $ERROR_INFO.message)
+          status = :error
         end
-        return executed_steps,status
+        [executed_steps, status]
       end
 
-      def run_step step,meta
+      ##
+      # Run a command associated with a step
+
+      # It sets the step's +status+ to +:warning+ if no command is associated
+      # with it. It sets the step's status to +:success+ if its +ignore+
+      # attribute is set.
+      def run_step(step, meta)
         if step.has_cmd? && step.cmd.respond_to?(:run)
           step.cmd.run(meta)
         else
           message("No command associated with step '#{step.step_type}'. Step number is #{step.number}")
-          step.status=:warning
+          step.status = :warning
         end
-        step.status=:success if step.ignore?
-        return step
+        step.status = :success if step.ignore?
+        step
       end
     end
 
-    class NoOp<Default
-      def run_step step,meta
+    ##
+    # Rutema::Runners::NoOp overrides the Rutema::Runners::Default#run_step
+    # method to make it non-operational
+    class NoOp < Default
+      ##
+      # Run a particular +step+ and return it afterwards
+      #
+      # Makes Rutema::Runners::Default#run_step a no-op. Like it it sets the
+      # step's +status+ to +:warning+ if no command is associated with it. It
+      # sets the step's status to +:success+ if its +ignore+ attribute is set.
+      def run_step(step, _meta)
         unless step.has_cmd? && step.cmd.respond_to?(:run)
           message("No command associated with step '#{step.step_type}'. Step number is #{step.number}")
-          step.status=:warning
+          step.status = :warning
         end
-        step.status=:success if step.ignore?
-        return step
+        step.status = :success if step.ignore?
+        step
       end
     end
   end
